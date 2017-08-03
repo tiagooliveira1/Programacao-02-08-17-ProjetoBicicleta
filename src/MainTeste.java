4ce4d0d7bc0ebd57a76df43a305bf4116b3c3eeb
Tiago Oliveira <tiago@econtabil.com>
2017-08-02 21:41:53 -0300

    Commit

--- src/MainTeste.java
+++ src/MainTeste.java
@@ -3,6 +3,10 @@ public class MainTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("início");
+		
+		System.out.println("fim");
 
 	}
 
